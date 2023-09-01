ed4931fe2b3017b0a366aa2d7f2dbb1b3301712a
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-09-01 13:35:56 -0700

    resolves

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,21 @@
 import requests
 import json
 
-class GetRequester:
 
-    def __init__(self, url):
+class GetRequester:
+    def __init__(
+        self,
+        url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json",
+    ):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        message_list = []
+        messages = json.loads(self.get_response_body())
+        for message in messages:
+            message_list.append(message)
+        return message_list
